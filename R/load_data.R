e1ec00299d351768188b64dc481ee917203fce18
Lukas Weber <lukas.weber.edu@gmail.com>
2020-08-03 11:56:22 -0400

    add mouse coronal dataset

--- R/load_data.R
+++ R/load_data.R
@@ -2,16 +2,15 @@
 #' 
 #' Temporary function to load data objects from Dropbox
 #' 
-#' Temporary function to load data objects from Dropbox before ExperimentHub
+#' Temporary function to load data objects from Dropbox until ExperimentHub
 #' package is set up
 #' 
 #' 
-#' @param data_name Name of dataset. Currently accepts "human_DLPFC".
+#' @param dataset_name Name of dataset. Either "human_DLPFC" or "mouse_coronal".
 #' 
-#' @param path Directory to save temporary file. Default = current working
-#'   directory.
+#' @param path Directory to save temporary files. Default = "tmp".
 #' 
-#' @param delete Whether to delete temporary file. Default = TRUE.
+#' @param delete Whether to delete temporary files. Default = TRUE.
 #' 
 #' 
 #' @return SingleCellExperiment or SpatialExperiment object
@@ -22,18 +21,29 @@
 #' @export
 #' 
 #' @examples
-#' spe <- load_data("human_DLPFC")
+#' #spe <- load_data("human_DLPFC")
+#' #spe <- load_data("mouse_coronal")
 #' 
-load_data <- function(data_name, path = ".", delete = TRUE) {
+load_data <- function(dataset_name, path = "tmp", delete = TRUE) {
   
-  match.arg(data_name, choices = c("human_DLPFC"))
+  match.arg(dataset_name, choices = c("human_DLPFC", "mouse_coronal"))
   
-  if (data_name == "human_DLPFC") {
+  # note: change "dl=0" to "dl=1" in link copied from Dropbox website to allow 
+  # downloading from command line
+  if (dataset_name == "human_DLPFC") {
     url <- "https://www.dropbox.com/s/b3s04x7il31g5qy/human_DLPFC_151673.RData?dl=1"
-    filename <- file.path(path, "human_DLPFC_151673.RData")
-    download.file(url, filename, mode = "wb")
-    load(filename)
-    if (delete) unlink(filename)
+  } else if (dataset_name == "mouse_coronal") {
+    url <- "https://www.dropbox.com/s/b3cu79rk7dkupa8/mouse_coronal.RData?dl=1"
+  }
+  
+  fn <- file.path(path, gsub("\\?.*$", "", basename(url)))
+  
+  dir.create(path)
+  download.file(url, fn, mode = "wb")
+  load(fn)
+  
+  if (delete) {
+    unlink(path, recursive = TRUE)
   }
   
   spe
---
+++ inst/scripts/make-data-mouse-coronal.R
@@ -0,0 +1,103 @@
+##########################################################
+# Script to create mouse coronal data object from raw data
+##########################################################
+
+# -------------
+# Download data
+# -------------
+
+# for more details on raw data see:
+# https://support.10xgenomics.com/spatial-gene-expression/datasets
+# https://support.10xgenomics.com/spatial-gene-expression/datasets/1.1.0/V1_Adult_Mouse_Brain
+
+url <- "https://cf.10xgenomics.com/samples/spatial-exp/1.1.0/V1_Adult_Mouse_Brain/V1_Adult_Mouse_Brain_filtered_feature_bc_matrix.tar.gz"
+fn <- basename(url)
+dir.create("tmp")
+download.file(url, file.path("tmp", fn))
+
+system(paste0("tar -C tmp -xvzf ", file.path("tmp", fn)))
+
+
+# ---------
+# Load data
+# ---------
+
+# download data matrix
+
+# for more details on matrix format used by Cell Ranger see:
+# https://support.10xgenomics.com/single-cell-gene-expression/software/pipelines/latest/output/matrices
+
+library(Matrix)
+
+matrix_dir <- file.path("tmp", "filtered_feature_bc_matrix")
+
+barcode_path <- file.path(matrix_dir, "barcodes.tsv.gz")
+features_path <- file.path(matrix_dir, "features.tsv.gz")
+matrix_path <- file.path(matrix_dir, "matrix.mtx.gz")
+
+mat <- readMM(file = matrix_path)
+
+feature_names <- read.delim(features_path, header = FALSE, stringsAsFactors = FALSE)
+barcode_names <- read.delim(barcode_path, header = FALSE, stringsAsFactors = FALSE)
+colnames(mat) <- barcode_names$V1
+rownames(mat) <- feature_names$V1
+
+
+# download spot coordinates
+
+url <- "https://cf.10xgenomics.com/samples/spatial-exp/1.1.0/V1_Adult_Mouse_Brain/V1_Adult_Mouse_Brain_spatial.tar.gz"
+fn <- basename(url)
+download.file(url, file.path("tmp", fn))
+
+system(paste0("tar -C tmp -xvzf ", file.path("tmp", fn)))
+
+spatial_dir <- file.path("tmp", "spatial")
+tissue_positions_path <- file.path(spatial_dir, "tissue_positions_list.csv")
+tissue_positions <- read.csv(tissue_positions_path, header = FALSE)
+
+coords <- tissue_positions[, c("V1", "V5", "V6")]
+colnames(coords) <- c("barcode", "x_coord", "y_coord")
+rownames(coords) <- coords$barcode
+dim(coords)
+
+
+# set up SingleCellExperiment
+
+# to do: use sparse matrix format
+
+library(SingleCellExperiment)
+
+# match x-y coordinates for spots containing data
+coords_matched <- coords[colnames(mat), ]
+dim(coords_matched)
+stopifnot(all(rownames(coords_matched) == colnames(mat)))
+
+# create SingleCellExperiment
+col_data <- DataFrame(coords_matched)
+row_data <- DataFrame(feature_names)
+colnames(row_data) <- c("gene_id", "gene_name", "type")
+
+spe <- SingleCellExperiment(
+  assays = list(counts = as.matrix(mat)),  ## to do: use sparse matrix format
+  rowData = row_data, 
+  colData = col_data
+)
+
+
+# ----------------------
+# Delete temporary files
+# ----------------------
+
+unlink("tmp", recursive = TRUE)
+
+
+# -----------
+# Save object
+# -----------
+
+# to do: store on ExperimentHub
+
+# for now: saving as publicly accessible Dropbox link
+
+save(spe, file = "~/Dropbox/STdata/mouse_coronal.RData")
+

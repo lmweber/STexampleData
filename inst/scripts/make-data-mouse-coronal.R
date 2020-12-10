79d6c7a60e34c9af7d54ac0ae1a8c26dce8d9670
Lukas Weber <lukas.weber.edu@gmail.com>
2020-12-10 16:42:37 -0500

    update mouse coronal object to SPE

--- inst/scripts/make-data-mouse-coronal.R
+++ inst/scripts/make-data-mouse-coronal.R
@@ -19,112 +19,133 @@ library(SpatialExperiment)
 # Download data
 # -------------
 
-dir.create("mouse_coronal")
+dir.create("tmp")
 
 # filtered feature-barcode matrix
+# note: filtered matrix contains only spots over tissue; raw matrix contains all spots
 url <- "https://cf.10xgenomics.com/samples/spatial-exp/1.1.0/V1_Adult_Mouse_Brain/V1_Adult_Mouse_Brain_filtered_feature_bc_matrix.tar.gz"
 fn <- basename(url)
-download.file(url, file.path("mouse_coronal", fn))
-system(paste0("tar -C mouse_coronal -xvzf ", file.path("mouse_coronal", fn)))
+download.file(url, file.path("tmp", fn))
+system(paste0("tar -C tmp -xvzf ", file.path("tmp", fn)))
 
 # spatial imaging data
 url <- "https://cf.10xgenomics.com/samples/spatial-exp/1.1.0/V1_Adult_Mouse_Brain/V1_Adult_Mouse_Brain_spatial.tar.gz"
 fn <- basename(url)
-download.file(url, file.path("mouse_coronal", fn))
-system(paste0("tar -C mouse_coronal -xvzf ", file.path("mouse_coronal", fn)))
+download.file(url, file.path("tmp", fn))
+system(paste0("tar -C tmp -xvzf ", file.path("tmp", fn)))
 
 
 # ---------
 # Load data
 # ---------
 
-# load features and barcodes
+# barcodes
+file_barcodes <- file.path("tmp", "filtered_feature_bc_matrix", "barcodes.tsv.gz")
+df_barcodes <- read.csv(file_barcodes, sep = "\t", header = FALSE, 
+                        col.names = c("barcode_id"))
 
-file_features <- file.path("mouse_coronal", "filtered_feature_bc_matrix", "features.tsv.gz")
-file_barcodes <- file.path("mouse_coronal", "filtered_feature_bc_matrix", "barcodes.tsv.gz")
+# features
+file_features <- file.path("tmp", "filtered_feature_bc_matrix", "features.tsv.gz")
+df_features <- read.csv(file_features, sep = "\t", header = FALSE, 
+                        col.names = c("gene_id", "gene_name", "feature_type"))
 
-df_features <- read.delim(file_features, header = FALSE, stringsAsFactors = FALSE)
-df_barcodes <- read.delim(file_barcodes, header = FALSE, stringsAsFactors = FALSE)
+# counts
+file_counts <- file.path("tmp", "filtered_feature_bc_matrix", "matrix.mtx.gz")
+counts <- readMM(file = file_counts)
 
-colnames(df_features) <- c("gene_id", "gene_name", "feature_type")
-colnames(df_barcodes) <- c("barcode_id")
+stopifnot(nrow(counts) == nrow(df_features))
+stopifnot(ncol(counts) == nrow(df_barcodes))
 
-rownames(df_features) <- df_features$gene_id
-rownames(df_barcodes) <- df_barcodes$barcode_id
+# spatial coordinates
+file_tisspos <- file.path("tmp", "spatial", "tissue_positions_list.csv")
+df_tisspos <- read.csv(file_tisspos, header = FALSE, 
+                       col.names=c("barcode_id", "in_tissue", "array_row", "array_col", 
+                                   "pxl_col_in_fullres", "pxl_row_in_fullres"))
 
-head(df_features)
-head(df_barcodes)
+# check dimensions
+dim(df_barcodes)
+dim(df_features)
+dim(counts)
+# note: df_tisspos contains all spots (not filtered), so need to match spots below
+dim(df_tisspos)
 
+# image file paths
+image_paths <- c(
+  aligned_fiducials = file.path("tmp", "spatial", "aligned_fiducials.jpg"), 
+  detected_tissue_image = file.path("tmp", "spatial", "detected_tissue_image.jpg"), 
+  tissue_hires_image = file.path("tmp", "spatial", "tissue_hires_image.png"), 
+  tissue_lowres_image = file.path("tmp", "spatial", "tissue_lowres_image.png")
+)
 
-# load feature-barcode matrix
-
-file_matrix <- file.path("mouse_coronal", "filtered_feature_bc_matrix", "matrix.mtx.gz")
-
-mat <- readMM(file = file_matrix)
-
-stopifnot(nrow(mat) == nrow(df_features))
-stopifnot(ncol(mat) == nrow(df_barcodes))
-
-rownames(mat) <- df_features$gene_id
-colnames(mat) <- df_barcodes$barcode_id
-
-
-# load spatial coordinates
-
-file_tissue_positions <- file.path("mouse_coronal", "spatial", "tissue_positions_list.csv")
-
-df_tissue_positions <- read.csv(file_tissue_positions, header = FALSE)
-
-# note array row corresponds to pixel column and vice versa
-colnames(df_tissue_positions) <- c("barcode_id", "in_tissue", 
-                                   "array_row", "array_col", 
-                                   "pxl_col_in_fullres", "pxl_row_in_fullres")
-
-rownames(df_tissue_positions) <- df_tissue_positions$barcode_id
-
-# keep spatial coordinates for barcodes that overlap with tissue
-df_tissue_positions_keep <- df_tissue_positions[df_barcodes$barcode_id, ]
-
-stopifnot(nrow(df_tissue_positions_keep) == nrow(df_barcodes))
-table(df_tissue_positions_keep$in_tissue)
-
-
-# load scale factors for image files
-
-file_scale_factors <- file.path("mouse_coronal", "spatial", "scalefactors_json.json")
-
+# spatial scale factors
+file_scale_factors <- file.path("tmp", "spatial", "scalefactors_json.json")
 scale_factors <- fromJSON(file = file_scale_factors)
 
 
-# paths to image files
+# ------------------------
+# Create SpatialExperiment
+# ------------------------
 
-image_paths <- c(
-  aligned_fiducials = file.path("mouse_coronal", "spatial", "aligned_fiducials.jpg"), 
-  detected_tissue_image = file.path("mouse_coronal", "spatial", "detected_tissue_image.jpg"), 
-  tissue_hires_image = file.path("mouse_coronal", "spatial", "tissue_hires_image.png"), 
-  tissue_lowres_image = file.path("mouse_coronal", "spatial", "tissue_lowres_image.png")
+# match and re-order barcode IDs (rows) in df_barcodes and df_tisspos
+dim(df_barcodes)
+dim(df_tisspos)
+ord <- match(df_barcodes$barcode_id, df_tisspos$barcode_id)
+df_tisspos_ord <- df_tisspos[ord, ]
+rownames(df_tisspos_ord) <- NULL
+
+# check
+dim(df_tisspos_ord)
+stopifnot(nrow(df_barcodes) == nrow(df_tisspos_ord))
+stopifnot(all(df_barcodes$barcode_id == df_tisspos_ord$barcode_id))
+head(df_barcodes)
+head(df_tisspos_ord)
+
+# row data
+row_data <- df_features
+
+# column data
+# note: add sample ID
+col_data <- df_barcodes
+col_data$sample_id <- "sample_01"
+
+# spatial coordinates
+# note: add "x_coord" and "y_coord" with flipped/reversed coordinates (standard orientation)
+spatial_coords <- df_tisspos_ord
+spatial_coords$x_coord <- spatial_coords$pxl_row_in_fullres
+spatial_coords$y_coord <- -1 * spatial_coords$pxl_col_in_fullres
+
+# image data
+# note: here we read in only one of the image files (low resolution)
+img_data <- readImgData(
+  path = file.path("tmp", "spatial"), 
+  sample_id = "sample_01", 
+  imageSources = image_paths["tissue_lowres_image"], 
+  scaleFactors = file_scale_factors, 
+  load = TRUE
 )
 
-
-# -----------------------
-# Create VisiumExperiment
-# -----------------------
-
-ve <- VisiumExperiment(
-  rowData = df_features, 
-  colData = df_barcodes, 
-  assays = list(counts = mat), 
-  spatialCoords = df_tissue_positions_keep, 
+# create SpatialExperiment
+spe <- SpatialExperiment(
+  assays = list(counts = counts), 
+  rowData = row_data, 
+  colData = col_data, 
+  spatialCoords = spatial_coords, 
   scaleFactors = scale_factors, 
-  imagePaths = image_paths
+  imgData = img_data, 
+  imageSources = img_paths, 
+  loadImage = TRUE, 
+  sample_id = "sample_01", 
+  image_id = "lowres"
 )
+
+spe
 
 
 # ----------------------
 # Delete temporary files
 # ----------------------
 
-unlink("mouse_coronal", recursive = TRUE)
+unlink("tmp", recursive = TRUE)
 
 
 # ----------------
@@ -135,5 +156,5 @@ unlink("mouse_coronal", recursive = TRUE)
 
 # for now: saving as publicly accessible Dropbox link
 
-save(ve, file = "~/Dropbox/SpaData/mouse_coronal.RData")
+save(spe, file = "~/Dropbox/SpaData/mouse_coronal.RData")
 

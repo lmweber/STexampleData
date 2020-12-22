2481cb728613eb5f071c2c66984f9ca86e82cb7e
Lukas Weber <lukas.weber.edu@gmail.com>
2020-12-22 17:24:35 -0500

    load seqFISH_mouseEmbryo dataset

--- R/load_data.R
+++ R/load_data.R
@@ -6,8 +6,8 @@
 #' Bioconductor's ExperimentHub).
 #' 
 #' 
-#' @param dataset_name Name of dataset. Currently accepts either "human_DLPFC"
-#'   or "mouse_coronal".
+#' @param dataset_name Name of dataset. Currently accepts "human_DLPFC",
+#'   "mouse_coronal", or "seqFISH_mouseEmbryo".
 #' 
 #' @param path Directory to save temporary files. Default = "tmp".
 #' 
@@ -24,16 +24,19 @@
 #' @examples
 #' #spe <- load_data("human_DLPFC")
 #' #spe <- load_data("mouse_coronal")
+#' #spe <- load_data("seqFISH_mouseEmbryo")
 #' 
 load_data <- function(dataset_name, path = "tmp", delete = TRUE) {
   
-  match.arg(dataset_name, choices = c("human_DLPFC", "mouse_coronal"))
+  match.arg(dataset_name, choices = c("human_DLPFC", "mouse_coronal", "seqFISH_mouseEmbryo"))
   
   # note: change "dl=0" to "dl=1" in link from Dropbox website to enable downloading
   if (dataset_name == "human_DLPFC") {
     url <- "https://www.dropbox.com/s/f26li36ov1aebl8/human_DLPFC.RData?dl=1"
   } else if (dataset_name == "mouse_coronal") {
     url <- "https://www.dropbox.com/s/qpbjgcl4o5q29b7/mouse_coronal.RData?dl=1"
+  } else if (dataset_name == "seqFISH_mouseEmbryo") {
+    url <- "https://www.dropbox.com/s/bit9odxqw6ugbuf/seqFISH_mouseEmbryo.RData?dl=1"
   }
   
   fn <- file.path(path, gsub("\\?.*$", "", basename(url)))

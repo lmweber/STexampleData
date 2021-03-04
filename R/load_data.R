21524ce53cd58e51fa868283c402c1cae8b88c41
Lukas Weber <lukas.weber.edu@gmail.com>
2021-03-04 17:49:54 -0500

    update download links for objects

--- R/load_data.R
+++ R/load_data.R
@@ -32,11 +32,11 @@ load_data <- function(dataset_name, path = "tmp", delete = TRUE) {
   
   # note: change "dl=0" to "dl=1" in link from Dropbox website to enable downloading
   if (dataset_name == "Visium_humanDLPFC") {
-    url <- "https://www.dropbox.com/s/9zllzxwmcjul8x8/Visium_humanDLPFC.RData?dl=1"
+    url <- "https://www.dropbox.com/s/u7ramj36ech7bwu/Visium_humanDLPFC.RData?dl=1"
   } else if (dataset_name == "Visium_mouseCoronal") {
-    url <- "https://www.dropbox.com/s/eut4gbwgxgbtni4/Visium_mouseCoronal.RData?dl=1"
+    url <- "https://www.dropbox.com/s/ilh0yk5n74ovbyu/Visium_mouseCoronal.RData?dl=1"
   } else if (dataset_name == "seqFISH_mouseEmbryo") {
-    url <- "https://www.dropbox.com/s/ei0aywt4kn005h0/seqFISH_mouseEmbryo.RData?dl=1"
+    url <- "https://www.dropbox.com/s/ajr6zli8d1o0fv8/seqFISH_mouseEmbryo.RData?dl=1"
   }
   
   fn <- file.path(path, gsub("\\?.*$", "", basename(url)))

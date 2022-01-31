290da04c6c262d29b0ad2716483bba87dc8f020c
Lukas Weber <lukas.weber.edu@gmail.com>
2022-01-30 21:25:09 -0500

    add accessors

--- R/load_data.R
+++ R/load_data.R
@@ -17,6 +17,8 @@
 #' # spe <- Visium_humanDLPFC()
 #' # spe <- Visium_mouseCoronal()
 #' # spe <- seqFISH_mouseEmbryo()
+#' # spe <- ST_mouseOB()
+#' # spe <- SlideSeqV2_mouseHPC()
 #' 
 #' @name load_data
 #' 
@@ -63,6 +65,42 @@ seqFISH_mouseEmbryo <- function() {
   
   # note: change "dl=0" to "dl=1" in link from Dropbox website to enable downloading
   url <- "https://www.dropbox.com/s/sfes5p3goxgr7zd/seqFISH_mouseEmbryo.rds?dl=1"
+  
+  path <- "tmp"
+  fn <- file.path(path, basename(url))
+  dir.create(path)
+  download.file(url, fn, mode = "wb")
+  
+  spe <- readRDS(fn)
+  
+  unlink(path, recursive = TRUE)
+  
+  spe
+}
+
+#' @rdname load_data
+ST_mouseOB <- function() {
+  
+  # note: change "dl=0" to "dl=1" in link from Dropbox website to enable downloading
+  url <- "https://www.dropbox.com/s/871mns9byabu4lm/ST_mouseOB.rds?dl=1"
+  
+  path <- "tmp"
+  fn <- file.path(path, basename(url))
+  dir.create(path)
+  download.file(url, fn, mode = "wb")
+  
+  spe <- readRDS(fn)
+  
+  unlink(path, recursive = TRUE)
+  
+  spe
+}
+
+#' @rdname load_data
+SlideSeqV2_mouseHPC <- function() {
+  
+  # note: change "dl=0" to "dl=1" in link from Dropbox website to enable downloading
+  url <- "https://www.dropbox.com/s/k1xp5tsfqq0ccco/SlideSeqV2_mouseHPC.rds?dl=1"
   
   path <- "tmp"
   fn <- file.path(path, basename(url))

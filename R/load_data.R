70ed6c431915ecaa4b3d9e5a41a598e91e5c7e18
Lukas Weber <lukas.weber.edu@gmail.com>
2022-03-04 12:47:16 -0500

    update download links

--- R/load_data.R
+++ R/load_data.R
@@ -25,7 +25,7 @@ NULL
 #' @rdname load_data
 Visium_humanDLPFC <- function() {
   # note: change "dl=0" to "dl=1" in link from Dropbox website to enable downloading
-  urlname <- "https://www.dropbox.com/s/lb5wfo6tskhfpdb/Visium_humanDLPFC.rds?dl=1"
+  urlname <- "https://www.dropbox.com/s/tu12bjsbodwkabo/Visium_humanDLPFC.rds?dl=1"
   spe <- readRDS(url(urlname))
   spe
 }
@@ -33,7 +33,7 @@ Visium_humanDLPFC <- function() {
 #' @rdname load_data
 Visium_mouseCoronal <- function() {
   # note: change "dl=0" to "dl=1" in link from Dropbox website to enable downloading
-  urlname <- "https://www.dropbox.com/s/wl7xb7edcjq8ski/Visium_mouseCoronal.rds?dl=1"
+  urlname <- "https://www.dropbox.com/s/3ch2vinhsvrp7m9/Visium_mouseCoronal.rds?dl=1"
   spe <- readRDS(url(urlname))
   spe
 }
@@ -41,7 +41,7 @@ Visium_mouseCoronal <- function() {
 #' @rdname load_data
 seqFISH_mouseEmbryo <- function() {
   # note: change "dl=0" to "dl=1" in link from Dropbox website to enable downloading
-  urlname <- "https://www.dropbox.com/s/oszugwkv5cldipe/seqFISH_mouseEmbryo.rds?dl=1"
+  urlname <- "https://www.dropbox.com/s/hahxamfkv4dkpeo/seqFISH_mouseEmbryo.rds?dl=1"
   spe <- readRDS(url(urlname))
   spe
 }
